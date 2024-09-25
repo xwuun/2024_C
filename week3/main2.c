c59b68d8b9be46ba53b8c90e005cd722051684c4
xwuun <soyun7305@naver.com>
2024-09-25 14:32:43 +0900

    Week3-2

---
+++ week3/main2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(void) {
+
+	char c;
+	int i;
+	
+	printf("input a number : ");
+	scanf("%c",&c);
+	
+	i=c-'0';
+	printf("The input number is %i\n",i);
+	
+	return 0;
+}
