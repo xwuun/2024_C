139a2382cf71aa9168d772582493ad44b09c8801
xwuun <soyun7305@naver.com>
2024-10-02 13:39:32 +0900

    week05_5

---
+++ week05/main5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int a,b;
+	
+	printf("Input two integers: ");
+	scanf("%d %d",&a,&b);
+	
+	printf("& result is %d\n",a&b);
+	printf("| result is %d\n",a|b);
+	printf("^ result is %d\n",a^b);
+	printf("<<1 result is %d\n",a<<1);
+	printf(">>1 result is %d\n",a>>1);
+	
+	return 0;
+}
