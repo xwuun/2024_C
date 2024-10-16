afdfa778047cf2f83f99cba55c1f61f1263c19c4
xwuun <soyun7305@naver.com>
2024-10-16 13:47:39 +0900

    week07_3

---
+++ week07/main3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+void square(int a)
+{
+	a =a*a;
+}
+
+int main()
+{
+	int a = 2;
+	square(a);
+	printf("a=%i\n",a);
+}*/
+
+int square( int a)
+{
+	return(a*a);
+}
+
+int main()
+{
+	int a= 2;
+	a = square(a);
+	printf("a=%i\n",a);
+}
