6bde25916b6b2dc2a321afc8170bd127bc4c4b1e
xwuun <soyun7305@naver.com>
2024-10-30 14:24:40 +0900

    week09_5

--- week09/main5.c
+++ week09/main5.c
@@ -9,3 +9,25 @@ int main(void)
 	printf("%d\n",all_files);
 	return 0;
 }
+
+
+/*static int sum = 1;
+int main(void)
+{
+	int sum = 0;
+	
+	printf("sum = %d\n",sum);
+	
+	return 0;
+}
+
+static int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n",all_files);
+	return 0;
+}*/
