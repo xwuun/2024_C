fbd4cf5c3181eb3315ba6f06f2136155be765c6b
xwuun <soyun7305@naver.com>
2024-10-30 14:19:10 +0900

    week09_5

---
+++ week09/main5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int all_files;
+
+extern void sub();
+
+int main(void)
+{
+	sub();
+	printf("%d\n",all_files);
+	return 0;
+}
---
+++ week09/sub.c
@@ -0,0 +1,12 @@
+extern int all_files;
+void sub(void)
+{
+	all_files = 10;
+}
+
+/*extern int sum;
+
+int sum1(void)
+{
+	sum = sum + 1;
+}*/
