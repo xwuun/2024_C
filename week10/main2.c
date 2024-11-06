12ad9780f7f050f9b0a1e079f1f2d2237fe246f0
xwuun <soyun7305@naver.com>
2024-11-06 13:56:53 +0900

    week10_2

--- week10/main2.c
+++ week10/main2.c
@@ -16,21 +16,4 @@ int main(void)
 		
 	return 0;
 }
-/*
-int main(void)
-{
-	int i,average;
-	int sum;
-	int grade[SIZE];
-	
-	sum=0;
-	for(i=0;i<SIZE;i++)
-	{
-		printf("학생 성적을 입력하세요 : ");
-		scanf("%d",&grade[i]);
-		sum += grade[i]; 
-	}
-	average= sum / SIZE;
-	printf("성적 평균 : %d\n",average);
-	return 0;
-}*/
+
