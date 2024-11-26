6ad2a6a96720351388422ab6a2b94ff3f19ae574
xwuun <soyun7305@naver.com>
2024-11-26 21:08:27 +0900

    week12_2

---
+++ week12/main2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+void main(void)
+{
+	int i = 4;
+
+	int *pi = &i;
+	char *pc = &i;
+	
+	printf("%i,%i,%i\n",i,*pi,*pc);
+}
+*/
+void main(void)
+{
+	int i = 300;
+
+	int *pi = &i;
+	char *pc = &i;
+	
+	printf("%i,%i,%i\n",i,*pi,*pc);
+}
