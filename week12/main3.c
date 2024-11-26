4577d47b231818a7a02bb5ec4092d044ca80e807
xwuun <soyun7305@naver.com>
2024-11-26 21:12:48 +0900

    week12_3

---
+++ week12/main3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+void swap (int x, int y)
+{
+	int temp;
+	temp = x;
+	x = y;
+	y = temp;
+}
+
+void main(void)
+{
+	int a = 3;
+	int b = 5;
+	swap(a,b);
+	printf("a:%i, b:%i\n",a,b);
+	
+}
+*/
+
+void swap (int *x, int *y)
+{
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+void main(void)
+{
+	int a = 3;
+	int b = 5;
+	swap(&a, &b);
+	printf("a:%i, b:%i\n",a,b);
+}
