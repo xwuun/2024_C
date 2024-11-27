4573a4a4887228aec9a11ca5e306d72819de627c
xwuun <soyun7305@naver.com>
2024-11-27 13:59:35 +0900

    week13_4

--- week13/main4.c
+++ week13/main4.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-
+/*
 struct point {
     int x; 
     int y; 
@@ -31,6 +31,44 @@ int main() {
 
     area = getArea(p1, p2);
 
+
+    printf("Area: %d\n", area);
+
+    return 0;
+}
+*/
+
+
+struct point
+{
+    int x;
+    int y; 
+};
+
+int getArea(struct point *pPtr1, struct point *pPtr2) {
+    int width = pPtr2->x - pPtr1->x;
+    int height = pPtr2->y - pPtr1->y; 
+
+    if (width < 0) width = -width;
+    if (height < 0) height = -height;
+
+    return width * height; 
+}
+
+int main() {
+    struct point p1, p2;     
+    struct point *pPtr1 = &p1; 
+    struct point *pPtr2 = &p2; 
+    int area; 
+
+
+    printf("Input the coordinate p1 (x y): ");
+    scanf("%d %d", &p1.x, &p1.y);
+
+    printf("Input the coordinate p2 (x y): ");
+    scanf("%d %d", &p2.x, &p2.y);
+
+    area = getArea(pPtr1, pPtr2);
 
     printf("Area: %d\n", area);
 
